16efb1ea4e399eccf35cb83aba28e5e04ce4d51e
Cihan <cihangonen35@gmail.com>
2021-07-14 11:35:32 +0300

    multer added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -12,8 +13,12 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/fileanalyse',(req,res)=>{
-  res.send(req.file);
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  res.json({
+    "name":req.file.originalname,
+    "type":req.file.mimetype,
+    "size":req.file.size
+  });
 })
 
 
